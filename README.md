6dcac089d2326f7ce079194d3c6125759d46e21d
leksanderOsnik <47179285+leksanderOsnik@users.noreply.github.com>
2022-06-06 21:59:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# d7c3a3c13a8cbee36de05
---
+++ src/test/java/com/genesys/example/GenesysCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesys.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesysCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
